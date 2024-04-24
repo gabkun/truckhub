05980668a125cee005e2a8298421efb0e4509cf0
gabkun <95224438+gabkun@users.noreply.github.com>
2024-04-24 15:55:16 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Components/Form/form'
+
+function App() {
+  return (
+    <>
+  <Form/>
+    </>
+  );
+}
+
+export default App;
